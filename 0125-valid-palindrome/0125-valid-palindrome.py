d8645457b70f25574599f4e97d751af8d5051948
김희윤ddd <99259398+heeyoonjik@users.noreply.github.com>
2024-07-28 16:27:40 +0900

    Time: 30 ms (60.53%), Space: 14.2 MB (17.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def isPalindrome(self, s):
-        # using deque
-        strs = collections.deque()
+        # using slicing
+        s = s.lower()
 
-        # preprocessing (because of Alphanumeric characters codition)
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
+        # filter out unnecessary character with regular expression
+        s = re.sub('[^a-z0-9]', '', s)
 
-        # is palindrome checker
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop():
-                return False
-
-        return True
+        return s == s[::-1] # slicing
         
